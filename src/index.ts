8b7317a83262dcc9e3b8cb387cfb622b7d7fd491
Ignacio de la Torre <ignaciodlta@gmail.com>
2025-08-16 17:49:58 -0600

    Fix index & add gitignore

--- src/index.ts
+++ src/index.ts
@@ -9,10 +9,3 @@ import * as _linux_arm64 from "@lazy_engineer/my-lib-linux-arm64";
 import * as _linux_x64 from "@lazy_engineer/my-lib-linux-x64";
 import * as _win32_arm64 from "@lazy_engineer/my-lib-win32-arm64";
 import * as _win32_x64 from "@lazy_engineer/my-lib-win32-x64";
-
-import { greet } from "./greet.ts";
-import { Person } from "./person.ts";
-
-console.log(greet("Mike"));
-const p = new Person("Alice", 30);
-console.log(p.greet("Mike"));

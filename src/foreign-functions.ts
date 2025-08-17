d9c4273eca7a9b38f016762fcbf39502964e7a45
Ignacio de la Torre <ignaciodlta@gmail.com>
2025-08-16 18:08:41 -0600

    Move imports

--- src/foreign-functions.ts
+++ src/foreign-functions.ts
@@ -2,6 +2,14 @@ import { Buffer } from "node:buffer";
 import { fileURLToPath } from "node:url";
 import * as e from "./generated/error.ts";
 
+// Import native build packages for each supported platform
+import * as _darwin_arm64 from "@lazy_engineer/my-lib-darwin-arm64";
+import * as _darwin_x64 from "@lazy_engineer/my-lib-darwin-x64";
+import * as _linux_arm64 from "@lazy_engineer/my-lib-linux-arm64";
+import * as _linux_x64 from "@lazy_engineer/my-lib-linux-x64";
+import * as _win32_arm64 from "@lazy_engineer/my-lib-win32-arm64";
+import * as _win32_x64 from "@lazy_engineer/my-lib-win32-x64";
+
 let ffi: typeof import("bun:ffi") 
 const isDeno = typeof Deno !== "undefined" && typeof Deno.version !== "undefined";
 const isBun = typeof Bun !== "undefined" && typeof Bun.version !== "undefined";
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,3 @@
 export { greet } from "./greet.ts";
 export { Person } from "./person.ts";
 export { close } from "./foreign-functions.ts"
-
-// Import native build packages for each supported platform
-import * as _darwin_arm64 from "@lazy_engineer/my-lib-darwin-arm64";
-import * as _darwin_x64 from "@lazy_engineer/my-lib-darwin-x64";
-import * as _linux_arm64 from "@lazy_engineer/my-lib-linux-arm64";
-import * as _linux_x64 from "@lazy_engineer/my-lib-linux-x64";
-import * as _win32_arm64 from "@lazy_engineer/my-lib-win32-arm64";
-import * as _win32_x64 from "@lazy_engineer/my-lib-win32-x64";
